04392a8fcf04da27b9a476cbba7c3fa9b9de424e
Joseph Jamolod <josephjamolod@gmail.com>
2024-12-22 18:29:55 +0800

    add log out functionality

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,9 +1,16 @@
 import { auth } from "@/auth";
+import SignOutBtn from "@/components/SignOutBtn";
+
 import React from "react";
 
 export default async function Settings() {
   const session = await auth();
   console.log(session);
 
-  return <div>{JSON.stringify(session)}</div>;
+  return (
+    <div>
+      {JSON.stringify(session)}
+      <SignOutBtn />
+    </div>
+  );
 }
---
+++ components/SignOutBtn.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+import { Button } from "./ui/button";
+import { logOut } from "@/actions/logout";
+
+export default function SignOutBtn() {
+  const handleSignOut = async () => await logOut();
+  return (
+    <div>
+      <Button variant={"destructive"} onClick={handleSignOut}>
+        Sign Out
+      </Button>
+    </div>
+  );
+}
