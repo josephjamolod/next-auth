ac48844f547e67eba9ad1b73fa21e728db8d6bfb
Joseph Jamolod <josephjamolod@gmail.com>
2024-12-22 15:38:29 +0800

    paste codes from next-auth docs

---
+++ auth.config.ts
@@ -0,0 +1,23 @@
+import bcrypt from "bcryptjs";
+import Credentials from "next-auth/providers/credentials";
+import type { NextAuthConfig } from "next-auth";
+import { loginSchema } from "./schemas";
+import { getUserByEmail } from "./data/user";
+
+export default {
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = loginSchema.safeParse(credentials);
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) return user;
+        }
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
