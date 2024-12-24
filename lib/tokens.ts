e5a6248671ce9ad7eb0fe5814efe4cb135e84679
Joseph Jamolod <josephjamolod@gmail.com>
2024-12-24 11:26:49 +0800

    create generate verification token fn

---
+++ lib/tokens.ts
@@ -0,0 +1,25 @@
+import { v4 as uuid } from "uuid";
+import { db } from "./db";
+import { getVerificationTokenByEmail } from "@/data/verification-token";
+
+export const generateToken = async (email: string) => {
+  try {
+    const token = uuid();
+    const expires = new Date(new Date().getTime() + 3600 * 1000);
+    const existingToken = await getVerificationTokenByEmail(email);
+
+    if (existingToken) {
+      await db.verificationToken.delete({
+        where: {
+          token: existingToken.token,
+        },
+      });
+    }
+    const verificationToken = await db.verificationToken.create({
+      data: { email, expires, token },
+    });
+    return verificationToken;
+  } catch (error) {
+    console.log(error);
+  }
+};
