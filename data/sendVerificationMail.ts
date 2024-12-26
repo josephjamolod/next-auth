9b9877c9e1edbd53069edc82e3f82f178852bee1
Joseph Jamolod <josephjamolod@gmail.com>
2024-12-26 19:32:37 +0800

    separate transport

--- data/sendVerificationMail.ts
+++ data/sendVerificationMail.ts
@@ -1,4 +1,4 @@
-import nodemailer from "nodemailer";
+import { transporter } from "@/lib/transporter";
 import { User, VerificationToken } from "@prisma/client";
 
 interface SendVerificationMailProp {
@@ -6,22 +6,12 @@ interface SendVerificationMailProp {
   token: VerificationToken;
 }
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: "josephjam627@gmail.com",
-    pass: process.env.EMAIL_PASS,
-  },
-});
-
 export const sendVerificationMail = ({
   user,
   token,
 }: SendVerificationMailProp) => {
   const mailOptions = {
-    from: '"Next-Auth" <josephjam627@gmail.com>', // sender address
+    from: `"Next-Auth" <${process.env.EMAIL}>`, // sender address
     to: user.email!, // list of receivers
     subject: "Verify your Email", // Subject line
     html: `<p>Hello ${user.name}, verify your email by clicking this link:</p>
