d7cacdffa24b5fd944f6ea77fab4f2539c4ef604
Joseph Jamolod <josephjamolod@gmail.com>
2024-12-26 20:10:01 +0800

    create generateResetPasswordToken

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,6 +1,7 @@
 import { v4 as uuid } from "uuid";
 import { db } from "./db";
 import { getVerificationTokenByEmail } from "@/data/verification-token";
+import { getResetPasswordTokenByEmail } from "@/data/reset-password-token";
 
 export const generateVerificationToken = async (email: string) => {
   try {
@@ -19,7 +20,27 @@ export const generateVerificationToken = async (email: string) => {
       data: { email, expires, token },
     });
     return verificationToken;
-  } catch (error) {
-    console.log(error);
+  } catch {
+    return null;
+  }
+};
+
+export const generateResetPasswordToken = async (email: string) => {
+  try {
+    const token = uuid();
+    const expires = new Date(new Date().getTime() + 3600 * 1000);
+    const existingToken = await getResetPasswordTokenByEmail(email);
+
+    if (existingToken) {
+      await db.resetPasswordToken.delete({
+        where: { token: existingToken.token },
+      });
+    }
+    const resetPasswordToken = await db.resetPasswordToken.create({
+      data: { email, expires, token },
+    });
+    return resetPasswordToken;
+  } catch {
+    return null;
   }
 };
