a63616b02dec7807a779f88a2e1ae5173d6e7b51
Joseph Jamolod <josephjamolod@gmail.com>
2024-12-26 20:16:23 +0800

    create reset password mail

---
+++ data/sendResetPasswordMail.ts
@@ -0,0 +1,28 @@
+import { transporter } from "@/lib/transporter";
+import { User, VerificationToken } from "@prisma/client";
+
+interface SendResetPasswordMailProp {
+  user: User;
+  token: VerificationToken;
+}
+
+export const sendResetPasswordMail = async ({
+  user,
+  token,
+}: SendResetPasswordMailProp) => {
+  const mailOptions = {
+    from: `"Next-Auth" <${process.env.EMAIL}>`, // sender address
+    to: user.email!, // list of receivers
+    subject: "Reset your password", // Subject line
+    html: `<p>Hello ${user.name}, reset your password by clicking this link:</p>
+    <a href="${process.env.CLIENT_URL}/auth/resetPassword/?token=${token.token}">Reset Your Password</a>`, // html body
+  };
+
+  transporter.sendMail(mailOptions, (error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email for reset password sent!");
+    }
+  });
+};
