2fea2ccf591c5fa7660355c999a983eae137cabd
Joseph Jamolod <josephjamolod@gmail.com>
2025-01-04 22:05:02 +0800

    add optional twoFactorCode in login schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -3,6 +3,7 @@ import * as z from "zod";
 export const loginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8, { message: "Password minimum 8 characters" }),
+  twoFactorCode: z.optional(z.string()),
 });
 
 export const registerSchema = z.object({
