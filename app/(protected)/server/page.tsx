a01c36051f0beeab55fd5246fec821e2735e6a3e
Joseph Jamolod <josephjamolod@gmail.com>
2025-01-06 08:44:23 +0800

    create server page

--- app/(protected)/server/page.tsx
+++ app/(protected)/server/page.tsx
@@ -1,11 +1,9 @@
+import UserInfo from "@/components/user-info";
 import { currentUser } from "@/lib/current-user";
 import React from "react";
 
 export default async function ServerPage() {
   const user = await currentUser();
-  console.log("User here");
 
-  console.log(user);
-
-  return <div>{JSON.stringify(user)}</div>;
+  return <UserInfo label="Server Page" user={user} />;
 }
